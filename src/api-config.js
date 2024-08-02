52513418611e189e4b6bd95d0b4dd4d296cdccb9
KimMinGyeol <alsruf741@naver.com>
2024-08-02 06:44:19 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else {
-  backendHost = "http://dev.aorlzld.p-e.kr";
+  backendHost = "https://dev.aorlzld.p-e.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
